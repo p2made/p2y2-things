650b7cb015bd39d5eb733486b75c849848ae760a
Pedro Plowman <pedrofp@me.com>
2016-03-17 22:57:55 +1000

    v1.3.4

---
+++ assets/Lightbox2Asset.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Lightbox2Asset.php
+ *
+ * @copyright Copyright &copy; Pedro Plowman, 2016
+ * @author Pedro Plowman
+ * @link https://github.com/p2made
+ * @package p2made/yii2-p2y2-things
+ * @license MIT
+ */
+
+namespace p2made\assets;
+
+class Lightbox2Asset extends \yii\web\AssetBundle
+{
+	private $resourceData = array(
+		'pub' => [
+			'sourcePath' => '@vendor/bower/lightbox2/dist',
+			'css' => [
+				'css/lightbox.min.css',
+			],
+			'js' => [
+				'js/lightbox.min.js',
+			],
+		],
+		'cdn' => [
+			'baseUrl' => '	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2',
+			'css' => [
+				'css/lightbox.min.css',
+			],
+			'js' => [
+				'js/lightbox.min.js',
+			],
+		],
+		'depends' => [
+		],
+	);
+
+	public function init()
+	{
+		$this->configureAsset($this->resourceData);
+		parent::init();
+	}
+}
+
+/*
+	@vendor/bower/lightbox2/dist/css/lightbox.min.css
+
+	@vendor/bower/lightbox2/dist/images/close.png
+	@vendor/bower/lightbox2/dist/images/loading.gif
+	@vendor/bower/lightbox2/dist/images/next.png
+	@vendor/bower/lightbox2/dist/images/prev.png
+
+	@vendor/bower/lightbox2/dist/js/lightbox-plus-jquery.min.js
+	@vendor/bower/lightbox2/dist/js/lightbox.min.js
+
+
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css
+
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/images/close.png
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/images/loading.gif
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/images/next.png
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/images/prev.png
+
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox-plus-jquery.min.js
+	//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js
+ */

ab0cb9238e6543914af3bcc4b6aa8b8252ce7a95
Pedro Plowman <p2opensource@gmail.com>
2017-02-08 22:53:56 +1000

    Update Copyright

--- helpers/base/BootstrapSocial.php
+++ helpers/base/BootstrapSocial.php
@@ -2,10 +2,11 @@
 /**
  * BootstrapSocial.php
  *
- * @copyright Copyright &copy; Pedro Plowman, 2016
- * @author Pedro Plowman * @license MIT
+ * @copyright Copyright &copy; Pedro Plowman, 2017
+ * @author Pedro Plowman
  * @link https://github.com/p2made
  * @package p2made/yii2-p2y2-things
+ * @license MIT
  */
 
 namespace p2made\helpers\base;

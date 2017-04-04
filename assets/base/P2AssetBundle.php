748e23d8049d16c0b6af0a4fdcbc82bcbae1bc4b
Pedro Plowman <p2opensource@gmail.com>
2017-04-05 02:16:36 +1000

    v2.3.0

--- assets/base/P2AssetBundle.php
+++ assets/base/P2AssetBundle.php
@@ -5,8 +5,10 @@
  * @copyright Copyright &copy; Pedro Plowman, 2017
  * @author Pedro Plowman
  * @link https://github.com/p2made
- * @package p2made/yii2-p2y2-things
  * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\base\P2AssetBundle
  */
 
 /**
@@ -19,6 +21,10 @@
  * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
  */
 
+namespace p2m\assets\base;
+
+use p2m\base\assets\P2AssetBase;
+
 /**
  * Load this asset with...
  * p2m\assets\base\P2AssetBundle::register($this);
@@ -26,37 +32,19 @@
  * or specify as a dependency with...
  *     'p2m\assets\base\P2AssetBundle',
  */
-
-namespace p2m\assets\base;
-
-/*
- * class p2m\assets\base\P2AssetBundle
- */
-class P2AssetBundle extends \yii\web\AssetBundle
+//class P2AssetBundle extends \p2m\base\assets\P2AssetBase
+class P2AssetBundle extends P2AssetBase
 {
 	/*
 	 * @var string
-	 * private $_p2mProjectId;
+	 * protected $version;
 	 */
-	protected $_p2mProjectId = 'yii2-p2y2-things';
 
 	/*
 	 * @var string
-	 * private $_p2mPath;
+	 * protected $_p2mProjectId;
 	 */
-	private $_p2mPath;
-
-	/*
-	 * @var boolean
-	 * private $_useStatic = false;
-	 */
-	private static $_useStatic;
-
-	/*
-	 * @var array | false
-	 * private $_staticEnd = false;
-	 */
-	private static $_staticEnd;
+	protected $_p2mProjectId = 'yii2-p2y2-things';
 
 	/**
 	 * @var string
@@ -89,129 +77,4 @@ class P2AssetBundle extends \yii\web\AssetBundle
 	 * @var array | false
 	 * public $_staticEnd = [] | false;
 	 */
-
-	protected function configureAsset($assetData)
-	{
-		if(isset($assetData['cssOptions'])) {
-			$this->cssOptions = $assetData['cssOptions'];
-		}
-		if(isset($assetData['jsOptions'])) {
-			$this->jsOptions = $assetData['jsOptions'];
-		}
-		if(isset($assetData['depends'])) {
-			$this->depends = $assetData['depends'];
-		}
-		if(isset($assetData['publishOptions'])) {
-			$this->publishOptions = $assetData['publishOptions'];
-		}
-
-		if(self::useStatic() && isset($assetData['static'])) {
-			$this->configureStaticAsset($assetData['static']);
-		} elseif(isset($assetData['published'])) {
-			$this->configurePublishedAsset($assetData['published']);
-		} else {
-			return;
-		}
-	}
-
-	protected function configureStaticAsset($assetData)
-	{
-		if(isset($assetData['baseUrl'])) {
-			$this->baseUrl = $assetData['baseUrl'];
-			$this->insertAssetVersion($this->baseUrl);
-		}
-		if(isset($assetData['css'])) {
-			$this->css = $assetData['css'];
-		}
-		if(isset($assetData['js'])) {
-			$this->js = $assetData['js'];
-		}
-	}
-
-	protected function configurePublishedAsset($assetData)
-	{
-		if(isset($assetData['sourcePath'])) {
-			$this->sourcePath = $assetData['sourcePath'];
-			$this->insertAssetVersion($this->sourcePath);
-			$this->insertP2mPath($this->sourcePath);
-		}
-
-		if(isset($assetData['css'])) {
-			$this->css = $assetData['css'];
-		}
-		if(isset($assetData['js'])) {
-			$this->js = $assetData['js'];
-		}
-	}
-
-	// ===== utility functions ===== //
-
-	protected function p2mPath()
-	{
-		if(isset($this->_p2mPath)) {
-			return $this->_p2mPath;
-		}
-
-		$this->_p2mPath = '@vendor/p2made/' . $this->_p2mProjectId . '/vendor';
-
-		return $this->_p2mPath;
-	}
-
-	protected function insertP2mPath(&$target)
-	{
-		$target = str_replace('@p2m@', $this->p2mPath(), $target);
-	}
-
-	protected function insertAssetVersion(&$target)
-	{
-		if(isset($this->version)) {
-			$target = str_replace('##-version-##', $this->version, $target);
-		}
-	}
-
-	/**
-	 * Get useStatic setting - use static resources
-	 * @return boolean
-	 * @default false
-	 */
-	protected static function useStatic()
-	{
-		if(isset(self::$_useStatic)) {
-			return self::$_useStatic;
-		}
-
-		// using ['p2m']['assets'] as param space
-		if(isset(\Yii::$app->params['p2m']['assets']['useStatic'])) {
-			self::$_useStatic = \Yii::$app->params['p2m']['assets']['useStatic'];
-		} else {
-			self::$_useStatic = false;
-		}
-
-		//self::$_useStatic = Settings::assetsUseStatic();
-
-		return self::$_useStatic;
-	}
-
-	/**
-	 * Get staticEnd setting - static application end
-	 * @return array | false
-	 * @default false
-	 */
-	protected static function staticEnd()
-	{
-		if(isset(self::$_staticEnd)) {
-			return self::$_staticEnd;
-		}
-
-		// using ['p2m']['assets'] as param space
-		if(isset(\Yii::$app->params['p2m']['assets']['staticEnd'])) {
-			self::$_staticEnd = \Yii::$app->params['p2m']['assets']['staticEnd'];
-		} else {
-			self::$_staticEnd = false;
-		}
-
-		//self::$_staticEnd = Settings::assetsStaticEnd();
-
-		return self::$_staticEnd;
-	}
 }

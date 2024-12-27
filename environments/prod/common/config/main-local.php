59092e54ffb383a5f754563f5c4228330380dd25
p2made <pedrofp@me.com>
2024-12-28 02:44:46 +1000

    add environments

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'components' => [
+		'db' => [
+			'class' => \yii\db\Connection::class,
+			'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		],
+		'mailer' => [
+			'class' => \yii\symfonymailer\Mailer::class,
+			'viewPath' => '@common/mail',
+		],
+	],
+];

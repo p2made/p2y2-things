0b87c0e6bd66f7c9614a0e7aa56691e3694240f6
p2made <pedrofp@me.com>
2025-04-01 06:14:58 +1000

    Create P2JqueryEasingAsset.php

---
+++ assets/P2JqueryEasingAsset.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * P2JqueryEasingAsset.php
+ *
+ * Yii2 asset for jQuery Easing
+ * https://github.com/gdsmith/jquery.easing
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\P2JqueryEasingAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\P2JqueryEasingAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\P2JqueryEasingAsset',
+ */
+
+namespace p2m\assets;
+
+use yii\web\AssetBundle;
+
+class P2JqueryEasingAsset extends AssetBundle
+{
+	public $sourcePath = null;
+
+	public $baseUrl = '//cdn.jsdelivr.net/npm/jquery.easing@1.4.1';
+
+	public $css = [
+		'jquery.easing.min.js',
+	];
+
+	public $cssOptions = [
+		'integrity' => 'sha256-H3cjtrm/ztDeuhCN9I4yh4iN2Ybx/y1RM7rMmAesA0k=',
+		'crossorigin' => 'anonymous',
+	];
+}

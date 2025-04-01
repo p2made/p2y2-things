0be08ed96d8d48a392172a0f63bf4c24f9c3b887
p2made <pedrofp@me.com>
2025-04-01 17:30:56 +1000

    Create P2DraggabillyAsset.php

---
+++ assets/P2DraggabillyAsset.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * P2DraggabillyAsset.php
+ *
+ * Yii2 asset for Draggabilly
+ * https://draggabilly.desandro.com/
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\P2DraggabillyAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\P2DraggabillyAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\P2DraggabillyAsset',
+ */
+
+namespace p2m\assets;
+
+use yii\web\AssetBundle;
+
+class P2DraggabillyAsset extends AssetBundle
+{
+	public $sourcePath = null;
+
+	public $baseUrl = '//cdn.jsdelivr.net/npm/draggabilly@3.0.0/';
+
+	public $js = [
+		'draggabilly.min.js',
+	];
+
+	public $jsOptions = [
+		'integrity' => 'sha384-4nAiCTVlq93vL1UaXiH8FDlZokfeoYM8mpb9/URzPuIfHvahTs8/gZxUimyaenOc',
+		'crossorigin' => 'anonymous',
+	];
+}

56bdb77e4a70511f82dcbab0fa772b64e2248954
p2made <pedrofp@me.com>
2025-04-02 22:09:26 +1000

    Create P2FullCalendarDaygridAsset.php

---
+++ assets/P2FullCalendarDaygridAsset.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * P2FullCalendarDaygridAsset.php
+ *
+ * Yii2 asset for FullCalendar/daygrid
+ * https://fullcalendar.io/
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\P2FullCalendarDaygridAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\P2FullCalendarDaygridAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\P2FullCalendarDaygridAsset',
+ */
+
+namespace p2m\assets;
+
+use yii\web\AssetBundle;
+
+class P2FullCalendarDaygridAsset extends AssetBundle
+{
+	public $sourcePath = null;
+
+	public $baseUrl = '//cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.15';
+
+	public $js = [
+		'index.global.min.js',
+	];
+
+	public $jsOptions = [
+		'integrity' => 'sha256-bxphyBuzK0CFtRG513b0dgkEVH/e0shTcEHdJWcjjf0=',
+		'crossorigin' => 'anonymous',
+	];
+
+	public $depends = [
+		'p2m\assets\P2FullCalendarAsset',
+	];
+}

110461ebb3e3c02c38d044e09cbb1165a311db43
p2made <pedrofp@me.com>
2025-04-02 21:40:45 +1000

    Rename P2FullCalendarGoogleCalendarAsset

--- assets/P2FullCalendarGoogleCalendarAsset.php
+++ assets/P2FullCalendarGoogleCalendarAsset.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * P2FullCalendarGCalAsset.php
+ * P2FullCalendarGoogleCalendarAsset.php
  *
  * Yii2 asset for FullCalendar/google-calendar
  * https://fullcalendar.io
@@ -11,22 +11,22 @@
  * @license MIT
  *
  * @package p2made/yii2-p2y2-things
- * @class \p2m\assets\P2FullCalendarGCalAsset
+ * @class \p2m\assets\P2FullCalendarGoogleCalendarAsset
  */
 
 /**
  * Load this asset with...
- * p2m\assets\P2FullCalendarGCalAsset::register($this);
+ * p2m\assets\P2FullCalendarGoogleCalendarAsset::register($this);
  *
  * or specify as a dependency with...
- *     'p2m\assets\P2FullCalendarGCalAsset',
+ *     'p2m\assets\P2FullCalendarGoogleCalendarAsset',
  */
 
 namespace p2m\assets;
 
 use yii\web\AssetBundle;
 
-class P2FullCalendarGCalAsset extends AssetBundle
+class P2FullCalendarGoogleCalendarAsset extends AssetBundle
 {
 	public $sourcePath = null;
 

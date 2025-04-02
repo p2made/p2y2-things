c35962f66db4cd578e08318c4b9390b00a11f558
p2made <pedrofp@me.com>
2025-04-03 05:34:24 +1000

    Create P2DataTablesResponsiveBaseAsset.php

---
+++ assets/base/P2DataTablesResponsiveBaseAsset.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * P2DataTablesResponsiveBaseAsset.php
+ *
+ * Yii2 base asset for DataTables/Responsive
+ * https://datatables.net
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\base\P2DataTablesResponsiveBaseAsset
+ */
+
+/**
+ * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
+ * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
+ * ##### ^ #####                                           ##### ^ #####
+ * ##### ^ #####      DO NOT USE THIS CLASS DIRECTLY!      ##### ^ #####
+ * ##### ^ #####                                           ##### ^ #####
+ * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
+ * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\base\P2DataTablesResponsiveBaseAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\base\P2DataTablesResponsiveBaseAsset',
+ */
+
+namespace p2m\assets\base;
+
+use yii\web\AssetBundle;
+
+class P2DataTablesResponsiveBaseAsset extends AssetBundle
+{
+	public $sourcePath = null;
+
+	public $baseUrl = '//cdn.datatables.net/responsive/3.0.4';
+
+	public $css = [
+		'css/responsive.bootstrap5.min.css',
+	];
+
+	public $cssOptions = [
+		'integrity' => 'sha384-seyUnB//1QOFEqox9uI7YTLBgz9jBwFRqZvsEPFrTw6NAsFEo70nhBWsQfODqiYA',
+		'crossorigin' => 'anonymous',
+	];
+
+	public $js = [
+		'js/dataTables.responsive.min.js',
+	];
+
+	public $jsOptions = [
+		'integrity' => 'sha384-A6In5tKqlvPZKDpH+ei4A3A4TZrEsyvvN2Fe+oCB1IaQfGD5HNqDIxwjztNKSGDd',
+		'crossorigin' => 'anonymous',
+	];
+
+	public $depends = [
+		'p2m\assets\P2DataTablesBootstrap5Asset',
+	];
+}

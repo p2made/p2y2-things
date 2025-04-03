9dbbafd3ddb6300d76d319833b3820d1821005ac
p2made <pedrofp@me.com>
2025-04-03 21:16:05 +1000

    Create P2TimelineAsset.php

---
+++ assets/P2TimelineAsset.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * P2TimelineAsset.php
+ *
+ * Yii2 asset for timeline.css
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-things
+ * @class \p2m\assets\P2TimelineAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\P2TimelineAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\P2TimelineAsset',
+ */
+
+namespace p2m\assets;
+
+use yii\web\AssetBundle;
+
+class P2TimelineAsset extends AssetBundle
+{
+	public $sourcePath = '@p2m/assets/lib';
+
+	public $css = [
+		'css/timeline.min.css',
+	];
+
+	public $depends = [
+		'p2m\assets\P2CoreAsset',
+	];
+}

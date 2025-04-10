787359839b360cc64210017162a1f6f78c26bd80
p2made <pedrofp@me.com>
2025-04-10 20:05:59 +1000

    Add initial files

---
+++ flags/FIS.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * BIS.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ */
+
+/**
+ * @class \p2m\icons\BIS
+ * @package p2made/yii2-p2y2-things
+ */
+
+namespace p2m\icons;
+
+class BS extends P2BootstrapIcons
+{
+	//public static $cssPrefix = 'bis';
+}

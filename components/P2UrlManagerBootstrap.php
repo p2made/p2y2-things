dcd73581395959888ecdcc61d4fad5bf15614d95
Pedro Plowman <pedrofp@me.com>
2025-12-06 21:00:15 +1000

    Update P2UrlManagerBootstrap.php

--- components/P2UrlManagerBootstrap.php
+++ components/P2UrlManagerBootstrap.php
@@ -8,6 +8,11 @@ class P2UrlManagerBootstrap implements BootstrapInterface
 {
 	public function bootstrap($app)
 	{
+		// Skip bootstrap when running in console
+		if (Yii::$app->request instanceof \yii\console\Request) {
+			return;
+		}
+
 		$scheme = Yii::$app->request->isSecureConnection ? 'https' : 'http';
 		$host = Yii::$app->request->hostName;
 

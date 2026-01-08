38be48bc0c44489a3ce1874aca38024ebfd5eb5c
Pedro Plowman <pedrofp@me.com>
2026-01-08 17:10:29 +1000

    Timezone

---
+++ helpers/TZ.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * TZ.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2025
+ * @link https://github.com/p2made
+ * @license MIT
+ */
+
+/**
+ * @class \p2m\helpers\TZ
+ * @package p2made/yii2-p2y2-things
+ */
+
+namespace p2m\helpers;
+
+use p2m\components\P2TimeZone;
+
+class TZ extends P2TimeZone
+{
+}
